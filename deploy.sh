bd49647349f18fdb6ee854d3f3daef14d7c0d949
Levente Löki <leventeloki@gmail.com>
2019-07-08 22:25:46 +0200

    Simplified deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-if [ "$HEROKU" != "true" ] || [ "$DEPLOY_STARTED" = "true" ]; then
+if [ "$HEROKU" != "true" ]; then
     exit 0
 fi
 
 echo "**************************"
 echo "**  Build on Heroku...  **"
 echo "**************************"
-
-export DEPLOY_STARTED="true"
 
 make compile
 make migrate_prod
