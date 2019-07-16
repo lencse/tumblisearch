b860d3877133d5451741febe56a548cf3418dbb9
Levente Löki <leventeloki@gmail.com>
2019-07-16 17:01:46 +0200

    Simplified config

--- config/server/config.ts
+++ config/server/config.ts
@@ -10,8 +10,6 @@ const config = {
     tumblrApiKey: String(env.TUMBLR_API_KEY),
     rabbitUrl: String(env.CLOUDAMQP_URL),
     queueName: 'tumblisearch',
-    runServer: 'true' === (env.RUN_SERVER || 'false').trim().toLowerCase(),
-    runQueue: 'true' === (env.RUN_QUEUE || 'false').trim().toLowerCase(),
     fetchPostCount: Number(env.FETCH_POST_COUNT),
     serverType: String(env.SERVER_TYPE || 'web')
 }
